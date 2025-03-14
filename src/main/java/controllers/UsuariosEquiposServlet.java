a2ee3284954bb436094a2ba73f6ef728100bd807
jhoandryA <jhoandryoficial@gmail.com>
2025-03-13 23:35:47 -0500

    creacion codigo servlet usuariosEquipos

--- src/main/java/controllers/UsuariosEquiposServlet.java
+++ src/main/java/controllers/UsuariosEquiposServlet.java
@@ -1,38 +1,46 @@
 package controllers;
 
 import java.io.IOException;
+
+
+
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UsuariosEquiposServlet
- */
+
+import models.UsuariosEquipos;
+import service.UsuariosEquiposService;
+import service.impl.UsuariosEquiposServiceImpl;
+
+
+
 @WebServlet("/UsuariosEquipos")
 public class UsuariosEquiposServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private final UsuariosEquiposService usuariosEquiposService;  
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public UsuariosEquiposServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        usuariosEquiposService = new UsuariosEquiposServiceImpl();
+        
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		List<UsuariosEquipos> usuariosEquipos = usuariosEquiposService.listarUsuariosEquipos();
+		request.setAttribute("usuariosEquipos", usuariosEquipos);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/views/UsuariosEquipos.jsp");
+		dispatcher.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
