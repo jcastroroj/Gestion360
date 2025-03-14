87489f161f7e6088236e57ccb039e156df4ee19b
jhoandryA <jhoandryoficial@gmail.com>
2025-03-13 23:13:31 -0500

    comint correccion y modificacion paquete dao

--- src/main/java/models/UsuariosEquipos.java
+++ src/main/java/models/UsuariosEquipos.java
@@ -1,5 +1,27 @@
 package models;
 
 public class UsuariosEquipos {
+	
+	 private String equipo;
+	    private String integrantes;
+
+	    public UsuariosEquipos() {}
+
+	    public String getEquipo() {
+	        return equipo;
+	    }
+
+	    public void setEquipo(String equipo) {
+	        this.equipo = equipo;
+	    }
+
+	    public String getIntegrantes() {
+	        return integrantes;
+	    }
+
+	    public void setIntegrantes(String integrantes) {
+	        this.integrantes = integrantes;
+	    }
+	
 
 }
