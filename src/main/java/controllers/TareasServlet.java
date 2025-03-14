b29deba00d1985d1e7598a3e0a8e9b4e55031d5f
jhoandryA <jhoandryoficial@gmail.com>
2025-03-13 19:30:22 -0500

    Modificacion del codigo (TareasServlet.java)

--- src/main/java/controllers/TareasServlet.java
+++ src/main/java/controllers/TareasServlet.java
@@ -1,33 +1,37 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class TareasServlet
- */
+import models.Tareas;
+import service.TareasService;
+import service.impl.TareasServiceImpl;
+
+
 @WebServlet("/Tareas")
 public class TareasServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private final TareasService tareasService;   
+   
     public TareasServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        tareasService = new TareasServiceImpl();
+        
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: prueba 2 ").append(request.getContextPath());
+		List<Tareas> tareas = tareasService.listarTareas();
+		request.setAttribute("tareas", tareas);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/views/Tareas.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
