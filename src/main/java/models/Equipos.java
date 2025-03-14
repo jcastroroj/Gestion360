0a061ec0c417865a28a27b2a32d1a7a50494f31c
jcastroroj <luisjcr.1801@gmail.com>
2025-03-13 22:03:09 -0500

    Codigo entidad Equipos

---
+++ src/main/java/models/Equipos.java
@@ -0,0 +1,32 @@
+package models;
+
+
+public class Equipos {
+
+	private Integer idEquipo;
+    private String nombre;
+    private String descripcion;
+	public Integer getIdEquipo() {
+		return idEquipo;
+	}
+	public void setIdEquipo(Integer idEquipo) {
+		this.idEquipo = idEquipo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+    
+    
+
+	
+	
+}
